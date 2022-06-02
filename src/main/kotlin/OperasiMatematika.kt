ceda5ae740600cf20f171ab0ef52ebeb8605825e
Rivaldo Rendy Mathindas <mathindas.r@gmail.com>
2022-06-02 23:06:35 +0700

    add operasi matematika

--- src/main/kotlin/OperasiMatematika.kt
+++ src/main/kotlin/OperasiMatematika.kt
@@ -1,6 +1,16 @@
 fun main(){
-
     val tambah = 2 + 1 + 2
+    val kurang = 2 - 1
+    val kali = 2 * 5
+    val bagi = 5.0 / 2
+    val modulus = 5 % 2
 
     println(tambah)
+    println(kurang)
+    println(kali)
+    println(bagi)
+    println(modulus)
+
+    val campuran = 1 + 2 * 3 / 3 - 1
+    println(campuran)
 }
